bd37dbe4a8e5fcd2e9d076c4e5273c919f6aa5fd
Christian Fries <email@christian-fries.de>
2020-04-19 10:54:48 +0200

    Added exercise on QuadraticEqueation

---
+++ src/main/java/com/christianfries/computerarithmetics/QuadraticEquationFromCoefficients.java
@@ -0,0 +1,7 @@
+package com.christianfries.computerarithmetics;
+
+public class QuadraticEquationFromCoefficients {
+
+	// Complete this implementation, then run the test QuadraticEuationFromCoefficientsTest (see src/test/java)
+
+}
---
+++ src/test/java/com/christianfries/computerarithmetics/QuadraticEuationFromCoefficientsTest.java
@@ -0,0 +1,31 @@
+package com.christianfries.computerarithmetics;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import info.quantlab.numericalmethods.lecture.computerarithmetics.check.QuadraticEquationImplemenationChecker;
+
+class QuadraticEuationFromCoefficientsTest {
+
+	@Test
+	void test() {
+		Class<?> classToTest = QuadraticEquationFromCoefficients.class;
+		
+		System.out.println("Testing " + classToTest.getCanonicalName());
+		
+		boolean success = QuadraticEquationImplemenationChecker.check(classToTest);
+
+		if(!success) {
+			System.out.println("Sorry, the test failed.");
+		}
+		else {
+			System.out.println("Congratulation! You solved the exercise.");
+		}
+
+		System.out.println("_".repeat(79));
+
+		if(!success) fail();
+	}
+
+}
