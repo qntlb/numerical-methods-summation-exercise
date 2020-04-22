045ee6ddc9e877b6a321bc858c2dbf700582d246
Christian Fries <email@christian-fries.de>
2020-04-22 10:52:56 +0200

    Added two separate test for accuracy.

--- src/test/java/com/christianfries/computerarithmetics/DoubleVectorFromArrayTest.java
+++ src/test/java/com/christianfries/computerarithmetics/DoubleVectorFromArrayTest.java
@@ -9,12 +9,32 @@ import info.quantlab.numericalmethods.lecture.computerarithmetics.check.DoubleVe
 public class DoubleVectorFromArrayTest {
 
 	@Test
-	void test() {
+	void testBasicFunctionality() {
 		Class<?> classToTest = DoubleVectorFromArray.class;
 		
 		System.out.println("Testing " + classToTest.getCanonicalName());
 		
-		boolean success = DoubleVectorImplemenationChecker.check(classToTest);
+		boolean success = DoubleVectorImplemenationChecker.check(classToTest, "basic");
+
+		if(!success) {
+			System.out.println("Sorry, the test failed.");
+		}
+		else {
+			System.out.println("Congratulation! You solved the exercise.");
+		}
+
+		System.out.println("_".repeat(79));
+
+		if(!success) fail();
+	}
+
+	@Test
+	void testAccuracy() {
+		Class<?> classToTest = DoubleVectorFromArray.class;
+		
+		System.out.println("Testing " + classToTest.getCanonicalName());
+		
+		boolean success = DoubleVectorImplemenationChecker.check(classToTest, "accuracy");
 
 		if(!success) {
 			System.out.println("Sorry, the test failed.");
