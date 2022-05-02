0dd3ea19950e70ac97b1c687f0469d36971a8098
Christian Fries <email@christian-fries.de>
2022-05-02 18:21:58 +0200

    Slimmer local test.

--- src/test/java/info/quantlab/numericalmethods/assignments/computerarithmetics/summation/SummationAssignmentTest.java
+++ src/test/java/info/quantlab/numericalmethods/assignments/computerarithmetics/summation/SummationAssignmentTest.java
@@ -11,41 +11,11 @@ public class SummationAssignmentTest {
 
 	@Test
 	void testBasicFunctionality() {
-		DoubleVectorFactory solution = new SummationAssignmentSolution();
-		
-		System.out.println("Testing basic functionality of " + solution.getClass().getCanonicalName());
-		
-		boolean success = DoubleVectorImplemenationChecker.check(solution, "basic");
-
-		if(!success) {
-			System.out.println("Sorry, the test failed.");
-		}
-		else {
-			System.out.println("Congratulation! You solved the basic part of the exercise.");
-		}
-
-		System.out.println("_".repeat(79));
-
-		if(!success) fail();
+		if(!DoubleVectorImplemenationChecker.check(new SummationAssignmentSolution(), "basic")) fail();
 	}
 
 	@Test
 	void testAccuracy() {
-		DoubleVectorFactory solution = new SummationAssignmentSolution();
-		
-		System.out.println("Testing accuracy of " + solution.getClass().getCanonicalName());
-		
-		boolean success = DoubleVectorImplemenationChecker.check(solution, "accuracy");
-
-		if(!success) {
-			System.out.println("Sorry, the test failed.");
-		}
-		else {
-			System.out.println("Congratulation! You solved the accuracy part of the exercise.");
-		}
-
-		System.out.println("_".repeat(79));
-
-		if(!success) fail();
+		if(!DoubleVectorImplemenationChecker.check(new SummationAssignmentSolution(), "accuracy")) fail();
 	}
 }
